5cbba0418db78f8f40ffb33cd44221a96d79e3c8
Linus Reimertz <linus.reimertz2@gmail.com>
2024-11-03 17:31:41 +0100

    started setting up demo

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 import "./style.css";
 
+const emojiEl = document.getElementById("emoji");
+const punchButton =
+  document.getElementById("punch");
+
 const emojis = [
-  "&#128515;",
-  "&#128578;",
-  "&#129301;"
+  "&#128565;",
+  "&#128555;",
+  "&#128551;",
+  "&#128543;",
+  "&#128578;"
 ];
+
+let currentHealth = [4];
+
+function punch() {
+  console.log("punch");
+  if (currentHealth > 0) {
+    currentHealth--;
+    updateDOM();
+  }
+}
+
+function updateDOM() {
+  emojiEl.innerHTML = emojis[currentHealth];
+}
+
+punchButton.onclick = punch;
